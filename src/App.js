c63341a39c0de7d73c92ed764cf3f274aa78b1e1
Scott Byrne <spbyrne@outlook.com>
2021-03-08 12:10:49 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+import { withTina, useForm, usePlugin } from "tinacms";
+
+const App = () => {
+  // create a Tina form
+  const [data, form] = useForm({
+    initialValues: {
+      title: "Tina Demo",
+      content: "Click the pencil icon to edit"
+    },
+    fields: [
+      {
+        name: "title",
+        component: "text"
+      },
+      {
+        name: "content",
+        component: "textarea"
+      }
+    ],
+    onSubmit: (values) => {
+      alert(
+        `You control what happens with your data \n --- \n ${JSON.stringify(
+          values,
+          null,
+          2
+        )}`
+      );
+    }
+  });
+
+  // register the Tina form so it appears in the sidebar
+  usePlugin(form);
+
+  // use `data` in your render to access form-mutated values
+  return (
+    <div className="App">
+      <h1>{data.title}</h1>
+      <h2>{data.content}</h2>
+    </div>
+  );
+};
+
+export default withTina(App, { enabled: true, sidebar: true });
