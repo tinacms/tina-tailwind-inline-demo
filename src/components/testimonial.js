c1132c80f561f87994bcc85db5e7fb8126582427
Scott Byrne <scott.byrne@forestry.io>
2021-03-26 10:05:48 -0300

    testimonial styles

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -10,13 +10,15 @@ export const Testimonial = () => {
   const theme = React.useContext(ThemeContext);
 
   return (
-    <section class={`py-24 bg-${theme.color}-800`}>
+    <section
+      class={`py-24 bg-${theme.color}-700 bg-gradient-to-r from-${theme.color}-700 via-${theme.color}-600 to-${theme.color}-700`}
+    >
       <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="relative">
           <blockquote>
-            <div class="relative z-10 max-w-3xl mx-auto text-center text-3xl leading-9 font-medium text-gray-100">
+            <div class="relative z-10 max-w-3xl mx-auto text-center text-4xl leading-tight font-medium text-gray-100">
               <span
-                class={`block text-black text-${theme.color}-700 text-8xl absolute inset-y-1/2 transform translate-y-2	-left-4 leading-4 -z-1`}
+                class={`block opacity-50 text-black text-${theme.color}-600 text-8xl absolute inset-y-1/2 transform translate-y-2	-left-4 leading-4 -z-1`}
               >
                 &ldquo;
               </span>
@@ -24,17 +26,19 @@ export const Testimonial = () => {
                 <InlineTextarea name="quote" />
               </p>
               <span
-                class={`block text-black text-${theme.color}-700 text-8xl absolute inset-y-1/2 transform translate-y-3	-right-4 leading-4 -z-1`}
+                class={`block opacity-50 text-black text-${theme.color}-600 text-8xl absolute inset-y-1/2 transform translate-y-3	-right-4 leading-4 -z-1`}
               >
                 &rdquo;
               </span>
             </div>
-            <footer class="mt-4 text-center">
-              <div className={`text-${theme.color}-700 mb-4 flex-grow-0`}>
-                —
-              </div>
+            <div className={`my-8 flex-grow-0`}>
+              <span
+                className={`block mx-auto h-0.5 w-1/6 bg-${theme.color}-800 opacity-50`}
+              ></span>
+            </div>
+            <footer class="text-center">
               <p
-                class={`tracking-wide text-black title-font text-${theme.color}-300`}
+                class={`tracking-wide title-font font-bold text-base text-${theme.color}-300`}
               >
                 <InlineTextarea name="author" />
               </p>
