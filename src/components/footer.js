c477cb160eb150353fbf24c069306ad1fae0d203
Scott Byrne <scott.byrne@forestry.io>
2021-03-26 14:08:46 -0300

    footer glow

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,93 +11,101 @@ export const Footer = ({ data, name = "" }) => {
   const theme = React.useContext(ThemeContext);
 
   return (
-    <footer class="text-white bg-gray-700 dark:bg-gray-800 body-font">
+    <footer class="overflow-hidden text-white bg-gray-700 dark:bg-gray-800 body-font">
       <InlineGroup
         name="footer"
         focusRing={{ offset: -16 }}
         insetControls={true}
         fields={FOOTER_FIELDS}
       >
-        <div className="container mx-auto flex pt-3">
-          <div class="w-full flex flex-col lg:flex-row flex-wrap py-12 px-12">
-            <div class="mb-8 lg:mr-48 flex-grow-0 inline-block">
-              <a class="">
-                <h2 class="text-lg font-bold tracking-tight text-white uppercase transition duration-500 ease-in-out transform hover:text-lightBlack-500">
-                  {name}
-                </h2>
-              </a>
+        <div className="relative">
+          <div className="relative container mx-auto flex pt-3 z-10">
+            <div class="w-full flex flex-col lg:flex-row flex-wrap py-12 px-12">
+              <div class="mb-8 lg:mr-48 flex-grow-0 inline-block">
+                <a class="">
+                  <h2 class="text-lg font-bold tracking-tight text-white uppercase transition duration-500 ease-in-out transform hover:text-lightBlack-500">
+                    {name}
+                  </h2>
+                </a>
+              </div>
+              <InlineBlocks
+                direction="horizontal"
+                className="flex-grow flex flex-wrap justify-between"
+                name="navlist"
+                blocks={FOOTER_BLOCKS}
+              />
             </div>
-            <InlineBlocks
-              direction="horizontal"
-              className="flex-grow flex flex-wrap justify-between"
-              name="navlist"
-              blocks={FOOTER_BLOCKS}
-            />
           </div>
-        </div>
-        <div class="bg-gray-800 dark:bg-gray-900">
-          <div class="container flex flex-col flex-wrap p-7 mx-auto sm:flex-row justify-center">
-            <div class="flex justify-center">
-              {data.social.facebook && (
-                <a
-                  href={data.social.facebook}
-                  class={`text-white hover:text-${theme.color}-500`}
-                >
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-6 h-6"
-                    viewBox="0 0 24 24"
+          <div className={`flex flex-row justify-center w-full`}>
+            <span
+              className={`absolute opacity-20 w-2/3 h-64 bg-${theme.color}-500 -bottom-48 rounded-t-full`}
+              style={{ filter: "blur(10rem)" }}
+            ></span>
+          </div>
+          <div class="bg-gray-800 dark:bg-gray-900 z-20 relative">
+            <div class="container flex flex-col flex-wrap p-7 mx-auto sm:flex-row justify-center">
+              <div class="flex justify-center">
+                {data.social.facebook && (
+                  <a
+                    href={data.social.facebook}
+                    class={`text-white hover:text-${theme.color}-500`}
                   >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </a>
-              )}
-              {data.social.twitter && (
-                <a
-                  href={data.social.twitter}
-                  class={`ml-4 text-white hover:text-${theme.color}-500`}
-                >
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-6 h-6"
-                    viewBox="0 0 24 24"
+                    <svg
+                      fill="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-6 h-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                  </a>
+                )}
+                {data.social.twitter && (
+                  <a
+                    href={data.social.twitter}
+                    class={`ml-4 text-white hover:text-${theme.color}-500`}
                   >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-              )}
-              {data.social.instagram && (
-                <a
-                  href={data.social.instagram}
-                  class={`ml-4 text-white hover:text-${theme.color}-500`}
-                >
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-6 h-6"
-                    viewBox="0 0 24 24"
+                    <svg
+                      fill="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-6 h-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                  </a>
+                )}
+                {data.social.instagram && (
+                  <a
+                    href={data.social.instagram}
+                    class={`ml-4 text-white hover:text-${theme.color}-500`}
                   >
-                    <rect
-                      width="20"
-                      height="20"
-                      x="2"
-                      y="2"
-                      rx="5"
-                      ry="5"
-                    ></rect>
-                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                  </svg>
-                </a>
-              )}
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-6 h-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <rect
+                        width="20"
+                        height="20"
+                        x="2"
+                        y="2"
+                        rx="5"
+                        ry="5"
+                      ></rect>
+                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         </div>
