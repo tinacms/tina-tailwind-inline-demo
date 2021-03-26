1cd1e9c09bde60e07e470feac0e592c924850629
Scott Byrne <scott.byrne@forestry.io>
2021-03-26 12:36:09 -0300

    bump social size

--- src/components/footer.js
+++ src/components/footer.js
@@ -36,7 +36,7 @@ export const Footer = ({ data, name = "" }) => {
           </div>
         </div>
         <div class="bg-gray-800 dark:bg-gray-900">
-          <div class="container flex flex-col flex-wrap px-5 py-6 mx-auto sm:flex-row justify-center">
+          <div class="container flex flex-col flex-wrap p-7 mx-auto sm:flex-row justify-center">
             <div class="flex justify-center">
               {data.social.facebook && (
                 <a
@@ -48,7 +48,7 @@ export const Footer = ({ data, name = "" }) => {
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    class="w-5 h-5"
+                    class="w-6 h-6"
                     viewBox="0 0 24 24"
                   >
                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -65,7 +65,7 @@ export const Footer = ({ data, name = "" }) => {
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    class="w-5 h-5"
+                    class="w-6 h-6"
                     viewBox="0 0 24 24"
                   >
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
@@ -83,7 +83,7 @@ export const Footer = ({ data, name = "" }) => {
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    class="w-5 h-5"
+                    class="w-6 h-6"
                     viewBox="0 0 24 24"
                   >
                     <rect
