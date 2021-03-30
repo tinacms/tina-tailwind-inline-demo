2ad68249407d2359f1ee13047c28429dcd705919
Fi1osof <n.lanets@freecode.academy>
2021-03-30 19:39:06 +0300

    Fix react warnings

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ export const Footer = ({ data, name = "" }) => {
   const theme = React.useContext(ThemeContext);
 
   return (
-    <footer class="text-white bg-gradient-to-br from-gray-700 to-gray-800 dark:from-gray-900 dark:to-gray-800 body-font">
+    <footer className="text-white bg-gradient-to-br from-gray-700 to-gray-800 dark:from-gray-900 dark:to-gray-800 body-font">
       <InlineGroup
         name="footer"
         focusRing={{ offset: -16 }}
@@ -20,10 +20,10 @@ export const Footer = ({ data, name = "" }) => {
       >
         <div className="relative">
           <div className="relative container mx-auto flex pt-8 lg:pt-3 z-10">
-            <div class="w-full flex flex-col lg:flex-row flex-wrap py-12 px-8">
-              <div class="mb-8 lg:mr-48 flex-grow-0 inline-block">
-                <a class="">
-                  <h2 class="text-lg font-bold tracking-tight text-white uppercase transition duration-500 ease-in-out transform hover:text-lightBlack-500">
+            <div className="w-full flex flex-col lg:flex-row flex-wrap py-12 px-8">
+              <div className="mb-8 lg:mr-48 flex-grow-0 inline-block">
+                <a className="">
+                  <h2 className="text-lg font-bold tracking-tight text-white uppercase transition duration-500 ease-in-out transform hover:text-lightBlack-500">
                     {name}
                   </h2>
                 </a>
@@ -47,20 +47,20 @@ export const Footer = ({ data, name = "" }) => {
           <div
             className={`relative w-full h-1 opacity-90 z-10 bg-gradient-to-r from-gray-800 via-${theme.color}-400 to-gray-800`}
           ></div>
-          <div class="bg-gray-800 dark:bg-gray-900 z-0 relative">
-            <div class="container flex flex-col flex-wrap p-7 mx-auto sm:flex-row justify-center">
-              <div class="flex justify-center">
+          <div className="bg-gray-800 dark:bg-gray-900 z-0 relative">
+            <div className="container flex flex-col flex-wrap p-7 mx-auto sm:flex-row justify-center">
+              <div className="flex justify-center">
                 {data.social.facebook && (
                   <a
                     href={data.social.facebook}
-                    class={`text-white hover:text-${theme.color}-500`}
+                    className={`text-white hover:text-${theme.color}-500`}
                   >
                     <svg
                       fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-6 h-6"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-6 h-6"
                       viewBox="0 0 24 24"
                     >
                       <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -70,14 +70,14 @@ export const Footer = ({ data, name = "" }) => {
                 {data.social.twitter && (
                   <a
                     href={data.social.twitter}
-                    class={`ml-4 text-white hover:text-${theme.color}-500`}
+                    className={`ml-4 text-white hover:text-${theme.color}-500`}
                   >
                     <svg
                       fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-6 h-6"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-6 h-6"
                       viewBox="0 0 24 24"
                     >
                       <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
@@ -87,15 +87,15 @@ export const Footer = ({ data, name = "" }) => {
                 {data.social.instagram && (
                   <a
                     href={data.social.instagram}
-                    class={`ml-4 text-white hover:text-${theme.color}-500`}
+                    className={`ml-4 text-white hover:text-${theme.color}-500`}
                   >
                     <svg
                       fill="none"
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-6 h-6"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-6 h-6"
                       viewBox="0 0 24 24"
                     >
                       <rect
@@ -123,25 +123,27 @@ export const FooterNavBlock = ({ index, data }) => {
   const theme = React.useContext(ThemeContext);
 
   return (
-    <div class="flex-grow px-6">
+    <div className="flex-grow px-6">
       <BlocksControls
         index={index}
         focusRing={{ offset: 16 }}
         insetControls={false}
       >
         <h3
-          class={`mb-3 text-sm font-semibold tracking-widest text-${theme.color}-400 uppercase title-font`}
+          className={`mb-3 text-sm font-semibold tracking-widest text-${theme.color}-400 uppercase title-font`}
         >
           <InlineTextarea name="title" />
         </h3>
-        <nav class="pb-8 list-none">
+        <nav className="pb-8 list-none">
           {data.items &&
             data.items.map(function (item, index) {
               return (
-                <li>
+                <li
+                  key={index}
+                >
                   <a
                     href={item.link}
-                    class="text-sm text-gray-200 hover:text-white"
+                    className="text-sm text-gray-200 hover:text-white"
                   >
                     {item.label}
                   </a>
--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -11,22 +11,22 @@ export const Testimonial = () => {
 
   return (
     <section
-      class={`py-24 bg-${theme.color}-700 bg-gradient-to-br from-${theme.color}-500 to-${theme.color}-700`}
+      className={`py-24 bg-${theme.color}-700 bg-gradient-to-br from-${theme.color}-500 to-${theme.color}-700`}
     >
-      <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="relative">
+      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="relative">
           <blockquote>
-            <div class="relative z-10 max-w-3xl mx-auto text-center text-4xl lg:text-5xl font-extrabold tracking-normal text-left title-font text-white">
+            <div className="relative z-10 max-w-3xl mx-auto text-center text-4xl lg:text-5xl font-extrabold tracking-normal text-left title-font text-white">
               <span
-                class={`block opacity-20 text-black text-${theme.color}-700 text-8xl absolute inset-y-1/2 transform translate-y-2	-left-4 leading-4 -z-1`}
+                className={`block opacity-20 text-black text-${theme.color}-700 text-8xl absolute inset-y-1/2 transform translate-y-2	-left-4 leading-4 -z-1`}
               >
                 &ldquo;
               </span>
-              <p class="relative opacity-95">
+              <p className="relative opacity-95">
                 <InlineTextarea name="quote" />
               </p>
               <span
-                class={`block opacity-20 text-black text-${theme.color}-800 text-8xl absolute inset-y-1/2 transform translate-y-3	-right-4 leading-4 -z-1`}
+                className={`block opacity-20 text-black text-${theme.color}-800 text-8xl absolute inset-y-1/2 transform translate-y-3	-right-4 leading-4 -z-1`}
               >
                 &rdquo;
               </span>
@@ -36,9 +36,9 @@ export const Testimonial = () => {
                 className={`block mx-auto h-0.5 w-1/6 bg-${theme.color}-700 opacity-70`}
               ></span>
             </div>
-            <footer class="text-center">
+            <footer className="text-center">
               <p
-                class={`tracking-wide title-font font-bold text-base text-${theme.color}-300`}
+                className={`tracking-wide title-font font-bold text-base text-${theme.color}-300`}
               >
                 <InlineTextarea name="author" />
               </p>
