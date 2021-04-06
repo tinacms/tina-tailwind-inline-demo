a16a843527d9c844eb62ad240a70d67e4784110f
Scott Byrne <scott.byrne@forestry.io>
2021-04-06 10:12:34 -0300

    expose tailwind colors as css custom props

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,5 +38,25 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    /* https://gist.github.com/Merott/d2a19b32db07565e94f10d13d11a8574 */
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = "") {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+
+          const newVars =
+            typeof value === "string"
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ":root": extractColorVars(theme("colors")),
+      });
+    },
+  ],
 };
